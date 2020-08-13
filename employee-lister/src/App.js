5f1ca220767489bcd1b22d6ffa83b5cf3ec7acc7
Jen Jayme <jeniferjayme@gmail.com>
2020-08-12 23:28:07 -0700

    Added table styling.

--- employee-lister/src/App.js
+++ employee-lister/src/App.js
@@ -2,31 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Container, Table } from 'react-bootstrap';
-import Gallery from '../src/components/gallery'
+// import Gallery from '../src/components/gallery'
+import EmployeeTable from '../src/components/table'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="../images/pawnee-seal.png" className="App-logo" alt="logo" />
       </header>
       <Container>
-        <Table>
         <div>
-          <Gallery />
+            <EmployeeTable />
         </div>
-        </Table>
       </Container>
 
     </div>
