fd01303c75f05e0e566177e46bf67900c4f6607e
Jen Jayme <jeniferjayme@gmail.com>
2020-08-14 22:54:59 -0700

    Added import statement to correct rendering of logo in header.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Container } from 'react-bootstrap';
 import Gallery from '../src/components/gallery'
 import EmployeeTable from '../src/components/table'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../src/components/pawnee-seal.png'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="../images/pawnee-seal.png" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>City of Pawnee Employee Directory</h1>
       </header>
       <Container>
